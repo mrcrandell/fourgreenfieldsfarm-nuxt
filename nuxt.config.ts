a05661f58042a92c4f4bcf802603c4d6a2152fa7
Matt Crandell <mrcrandell@gmail.com>
2025-08-24 22:09:11 -0400

    added contact endpoint and some validation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,32 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/fonts"],
+  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/turnstile"],
 
   runtimeConfig: {
     jwtSecret: process.env.JWT_SECRET || "your_jwt_secret",
+    mailgunApi: process.env.MAILGUN_API_KEY || "",
+    mailgunDomain: process.env.MAILGUN_DOMAIN || "",
+    nuxtTurnstileSecretKey: process.env.NUXT_TURNSTILE_SECRET_KEY || "",
     public: {
-      // Public runtime config
+      turnstile: {
+        siteKey: process.env.NUXT_PUBLIC_TURNSTILE_SITE_KEY || "",
+      },
     },
   },
 
   vite: {
-    define: {
-      global: "globalThis",
-    },
     optimizeDeps: {
       exclude: ["@prisma/client"],
-    },
-  },
-
-  nitro: {
-    experimental: {
-      wasm: true,
-    },
-    esbuild: {
-      options: {
-        target: "esnext",
-      },
     },
   },
 });
