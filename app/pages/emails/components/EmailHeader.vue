56a1e4c72f57c81635a37773041df55f9f5c0edf
Matt Crandell <mrcrandell@gmail.com>
2025-08-24 20:55:40 -0400

    added vue email compiler

---
+++ app/pages/emails/components/EmailHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="header">
+    <img src="https://www.fourgreenfieldsfarm.com/img/four-green-fields-farm-logo-email.jpg" alt="Four Green Fields Farm" />
+  </header>
+</template>
+
+<style>
+header.header {
+  display: block;
+  max-width: 600px;
+}
+header.header img {
+  width: 100%;
+  height: auto;
+  display: block;
+}
+</style>
